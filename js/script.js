5e9371c920882b777b5856163a007f8d29a2271d
victor.briand <82378634+victorBriand@users.noreply.github.com>
2021-05-10 21:03:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var windowWidth = window.innerWidth;
+
+    if (windowWidth > 500) {
+    $("#apropos").hover(function() {
+        $("#rondB").animate({ opacity: "1" }, 800, "swing" );
+        $("#description").delay(1000).animate({ opacity: "1" }, 800, "swing" );
+    });
+}
+    // $("#langages").on('mouseenter', function () { 
+    //     if (!$("#fr, #pt, #en, #sp, #ger").hasClass('animationDone')) {
+    //       $("#fr, #pt").animate({
+    //         width: "100%"
+    //       }, 500, function() {
+    //         $("#fr, #pt").addClass('animationDone');
+    //       });
+    //       $("#en, #sp").animate({
+    //         width: "80%"
+    //       }, 500, function() {
+    //         $("#en, #sp").addClass('animationDone');
+    //       });
+    //       $("#ger").animate({
+    //         width: "20%"
+    //       }, 500, function() {
+    //         $("#ger").addClass('animationDone');
+    //       });
+    //     }
+    //   });
+});
